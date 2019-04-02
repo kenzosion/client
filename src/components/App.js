90cfda4a9bf92a2e31e1e39b4f5e320820154108
Haur Kang <haur.kang@gmail.com>
2019-04-02 22:22:35 +0100

    created header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BrowserRoute, Route, Link } from 'react-router-dom';
-import { Stream } from 'stream';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import  StreamCreate  from "./streams/StreamCreate";
+import  StreamDelete  from "./streams/StreamDelete";
+import  StreamEdit  from "./streams/StreamEdit";
+import  StreamList  from './streams/StreamList';
+import  StreamShow  from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
     return (
       <div>
-        <BrowserRoute>
+        <BrowserRouter>
           <div>
-            <Route path="/" exact component={StreamList} />
-            <Route path="/" exact component={Stream} />
-            <Route path="/" exact component={Stream} />
-            <Route path="/" exact component={Stream} />
-            <Route path="/" exact component={Stream} />
+            <Header />
+            <Route path="/" exact component={ StreamList } />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
           </div>
-        </BrowserRoute>
+        </BrowserRouter>
       </div>
     );
 }
