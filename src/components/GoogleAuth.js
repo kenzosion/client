cd574fb356515eb4b4a32e8cfa696fea842f067d
Haur Kang <haur.kang@gmail.com>
2019-04-04 18:04:26 +0100

    styled google auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 class GoogleAuth extends Component {
+    state = { isSignedIn: null };
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', ()=> {
+            window.gapi.client.init({
+               clientId:
+                "779340881055-q5jf5gomlsao8ok8sohfafhe6r9ejf3d.apps.googleusercontent.com",
+               scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+        });
+    }
+
+  renderAuthButton() {
+      if (this.state.isSignedIn === null ){
+          return <div>I do not know if we are signed in</div>;
+      } else if (this.state.isSignedIn) {
+          return <div>I am signed in</div>;
+      } else {
+          return <div>I am not signed in</div>;
+      }
+  }
   render() {
     return (
       <div>
-        google auth
+        {this.renderAuthButton()}
       </div>
     )
   }
