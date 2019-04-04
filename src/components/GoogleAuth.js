cee127976edeb9ff47202f470325da66a286f96c
Haur Kang <haur.kang@gmail.com>
2019-04-04 16:27:13 +0100

    added google auth component into header component

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class GoogleAuth extends Component {
+  render() {
+    return (
+      <div>
+        google auth
+      </div>
+    )
+  }
+}
+export default GoogleAuth;
