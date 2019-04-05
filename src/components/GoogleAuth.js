b34211995727cdf9e2254654d24756c6f751adfe
Haur Kang <haur.kang@gmail.com>
2019-04-05 14:33:31 +0100

    created authreducers

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends Component {
     state = { isSignedIn: null };
@@ -17,9 +19,13 @@ class GoogleAuth extends Component {
         });
     }
 
-onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get()});
-}
+onAuthChange = (isSignedIn) => {
+    if (isSignedIn){
+        this.props.signIn();
+    } else {
+        this.props.signOut();
+    }
+};
 
 onSignInClick = () => {
     this.auth.signIn();
@@ -59,4 +65,4 @@ onSignOutClick = () => {
     )
   }
 }
-export default GoogleAuth;
+export default connect(null, {signIn,signOut})(GoogleAuth);
