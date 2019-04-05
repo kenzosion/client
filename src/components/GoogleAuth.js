266b07734c1f182cc7030967edc59f1a2fa10ef6
Haur Kang <haur.kang@gmail.com>
2019-04-05 14:51:16 +0100

    adds types.js file

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends Component {
-    state = { isSignedIn: null };
-
     componentDidMount() {
         window.gapi.load('client:auth2', ()=> {
             window.gapi.client.init({
@@ -13,7 +11,7 @@ class GoogleAuth extends Component {
                scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
         });
         });
@@ -36,9 +34,9 @@ onSignOutClick = () => {
 }
 
   renderAuthButton() {
-      if (this.state.isSignedIn === null ){
+      if (this.props.isSignedIn === null ){
           return null;
-      } else if (this.state.isSignedIn) {
+      } else if (this.props.isSignedIn) {
           return (
               <button onClick={this.onSignOutClick} className="ui red google button">
                 <i className="google icon"></i>
@@ -65,4 +63,11 @@ onSignOutClick = () => {
     )
   }
 }
-export default connect(null, {signIn,signOut})(GoogleAuth);
+
+const mapStateToProps = state => {
+    return { isSignedIn: state.auth.isSignedIn};
+}
+export default connect(
+  mapStateToProps,
+  { signIn, signOut }
+)(GoogleAuth);
