f9b024ca2a8781b373fc44f3d031ca24b6c5fbfe
Haur Kang <haur.kang@gmail.com>
2019-04-06 20:35:39 +0100

    added new reducer, formReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers } from 'redux';
 import authReducers from './authReducers';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
-    auth: authReducers
+    auth: authReducers,
+    form: formReducer
 });
