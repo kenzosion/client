a41b87ab84b5da62fe53635ef539b54393cad9ef
Haur Kang <haur.kang@gmail.com>
2019-04-06 21:05:04 +0100

    wired input and field

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
+  renderInput({input}) {
+    return (
+        <input {...input} />
+    )
+  };
+  
   render() {
     return (
       <form>
-        <Field />
+        <Field name="title" component={this.renderInput}/>
+        <Field name="description" component={this.renderInput}/>
       </form>
     )
-  }
+  } 
 }
 
 export default reduxForm({
