9da65ac600dfbc6ca8390c4c43d8d21f203b82a3
Haur Kang <haur.kang@gmail.com>
2019-04-10 22:21:48 +0100

    created fetch_streams action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,37 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
-import streams from '../apis/streams';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  DELETE_STREAM,
+  FETCH_STREAM,
+  EDIT_STREAM
+} from "./types";
+import streams from "../apis/streams";
 
-
-
-export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    };
+export const signIn = userId => {
+  return {
+    type: SIGN_IN,
+    payload: userId
+  };
 };
 
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    };
+  return {
+    type: SIGN_OUT
+  };
 };
 
 //use streams as axios
 export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues);
+  const response = await streams.post('/streams', formValues);
 
-    dispatch ({ type: CREATE_STREAM, payload: response.data});
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
