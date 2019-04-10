039752661daeddaaaca75af669cac96a29442caf
Haur Kang <haur.kang@gmail.com>
2019-04-10 22:10:41 +0100

    created 4 more action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
 import streams from '../apis/streams';
 
 
+
 export const signIn = (userId) => {
     return {
         type: SIGN_IN,
@@ -15,9 +16,10 @@ export const signOut = () => {
     };
 };
 
+//use streams as axios
 export const createStream = formValues => async dispatch => {
     const response = await streams.post('/streams', formValues);
 
     dispatch ({ type: CREATE_STREAM, payload: response.data});
 };
- 
+
