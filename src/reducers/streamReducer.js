4efb8e5bac4296b96a18c4096f67cf323f49dd39
Haur Kang <haur.kang@gmail.com>
2019-04-11 17:33:43 +0100

    installed lodash

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -8,6 +8,20 @@ export default (state= {}, action ) => {
     switch (action.type) {
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+    
+        default:
+            return state;
     }
-
 };
+
+
+
+export default { state={}, action } => {
+    switch (action.type) {
+
+    }
+}
