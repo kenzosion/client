a10046b68838a416247f03bcc5223ea482b7d4ef
Haur Kang <haur.kang@gmail.com>
2019-04-15 22:27:43 +0100

    added history.push

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ import {
   EDIT_STREAM
 } from "./types";
 import streams from "../apis/streams";
+import history from '../history';
 
 export const signIn = userId => {
   return {
@@ -28,6 +29,8 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', {...formValues, userId} );
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  //Do some programmatic navigation 
+  history.push('/');
 };
 
 //fetching a list 
